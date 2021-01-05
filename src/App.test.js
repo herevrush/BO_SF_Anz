44123750c6f38aec445a3596c64ae2996619c42f
herevrush <herevrush@yahoo.com>
2021-01-05 20:06:01 +1100

    moving code

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import { render, fireEvent, cleanup, wait } from '@testing-library/react';
 import fetchMock from 'fetch-mock'
 import 'jest-dom/extend-expect';
 
-const renderApp = () => render(<App/>);
+const renderApp = () => render(<App />);
 
 afterEach(() => {
   fetchMock.restore();
@@ -12,20 +12,20 @@ afterEach(() => {
 });
 
 test('initial UI is rendered as expected', () => {
-	let { getByTestId, queryByTestId } = renderApp();
+  let { getByTestId, queryByTestId } = renderApp();
   expect(getByTestId('app-title')).toHaveTextContent('Football Comptetions');
   expect(getByTestId('year-list').childNodes).toHaveLength(7);
-	expect(queryByTestId('total-matches')).toBe(null);
+  expect(queryByTestId('total-matches')).toBe(null);
   expect(queryByTestId('match-list')).toBe(null);
   expect(queryByTestId('no-result')).toBe(null);
 });
 
-test('search is made on by clicking on search button and no results found', async() => {
-	let { getByTestId, queryByTestId } = renderApp();
+test('search is made on by clicking on search button and no results found', async () => {
+  let { getByTestId, queryByTestId } = renderApp();
 
   const url = 'https://jsonmock.hackerrank.com/api/football_competitions?year=2017';
-  fetchMock.getOnce(url, JSON.stringify({ page:1,per_page:10,total:0,total_pages:0,data:[]}));
-  
+  fetchMock.getOnce(url, JSON.stringify({ page: 1, per_page: 10, total: 0, total_pages: 0, data: [] }));
+
   fireEvent.click(getByTestId('year-list').childNodes[6]);
 
   await wait(() => {
@@ -36,12 +36,12 @@ test('search is made on by clicking on search button and no results found', asyn
   });
 });
 
-test('search is made on by clicking on search button and result found - test 1', async() => {
-	let { getByTestId, queryByTestId } = renderApp();
+test('search is made on by clicking on search button and result found - test 1', async () => {
+  let { getByTestId, queryByTestId } = renderApp();
 
   const url = 'https://jsonmock.hackerrank.com/api/football_competitions?year=2011';
-  fetchMock.getOnce(url, JSON.stringify({ page:1,per_page:10,total:0,total_pages:0,data:[{"name":"UEFA Champions League","country":"","year":2011,"winner":"Chelsea","runnerup":"Bayern Munich"},{"name":"Serie A","country":"Italy","year":2011,"winner":"Juventus","runnerup":"AC Milan"},{"name":"Bundesliga","country":"Germany","year":2011,"winner":"Borussia Dortmund","runnerup":"Bayern Munchen"}]}));
-  
+  fetchMock.getOnce(url, JSON.stringify({ page: 1, per_page: 10, total: 0, total_pages: 0, data: [{ "name": "UEFA Champions League", "country": "", "year": 2011, "winner": "Chelsea", "runnerup": "Bayern Munich" }, { "name": "Serie A", "country": "Italy", "year": 2011, "winner": "Juventus", "runnerup": "AC Milan" }, { "name": "Bundesliga", "country": "Germany", "year": 2011, "winner": "Borussia Dortmund", "runnerup": "Bayern Munchen" }] }));
+
   fireEvent.click(getByTestId('year-list').childNodes[0]);
 
   await wait(() => {
@@ -55,12 +55,12 @@ test('search is made on by clicking on search button and result found - test 1',
   });
 });
 
-test('search is made on by clicking on search button and result found - test 2', async() => {
-	let { getByTestId, queryByTestId } = renderApp();
+test('search is made on by clicking on search button and result found - test 2', async () => {
+  let { getByTestId, queryByTestId } = renderApp();
 
   const url = 'https://jsonmock.hackerrank.com/api/football_competitions?year=2013';
-  fetchMock.getOnce(url, JSON.stringify({ page:1,per_page:10,total:0,total_pages:0,data:[{"name":"English Premier League","country":"England","year":2013,"winner":"Manchester City","runnerup":"Liverpool"},{"name":"La Liga","country":"Spain","year":2013,"winner":"Atletico Madrid","runnerup":"FC Barcelona"}]}));
-  
+  fetchMock.getOnce(url, JSON.stringify({ page: 1, per_page: 10, total: 0, total_pages: 0, data: [{ "name": "English Premier League", "country": "England", "year": 2013, "winner": "Manchester City", "runnerup": "Liverpool" }, { "name": "La Liga", "country": "Spain", "year": 2013, "winner": "Atletico Madrid", "runnerup": "FC Barcelona" }] }));
+
   fireEvent.click(getByTestId('year-list').childNodes[2]);
 
   await wait(() => {
