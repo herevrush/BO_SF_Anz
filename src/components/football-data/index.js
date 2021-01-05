f832e0147e9bed88be380081e32a53a3fcb013cf
herevrush <herevrush@yahoo.com>
2021-01-05 19:15:21 +1100

    added code to display data

--- src/components/football-data/index.js
+++ src/components/football-data/index.js
@@ -8,7 +8,10 @@ export default class FootballMatchesData extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedYear: null
+      selectedYear: null,
+      matches: [],
+      totalMatches: null,
+
     };
   }
 
@@ -16,11 +19,31 @@ export default class FootballMatchesData extends Component {
     // Code written in next line is to take care of adding active class to selected year for css purpose.
     this.setState({
       selectedYear: year
-    })
+    }, this.getFootballData(year));
+  }
+
+  getFootballData = (year) => {
+    fetch(`https://jsonmock.hackerrank.com/api/football_competitions?year=${year}`)
+      .then((response) => response.json())
+      .then(data => this.setState({
+        matches: data.data,
+        totalMatches: data.total
+      }));
+
   }
 
   render() {
-    var years= [2011, 2012, 2013, 2014, 2015, 2016, 2017];
+    var years = [2011, 2012, 2013, 2014, 2015, 2016, 2017];
+    let listElements = (<li className="slide-up-fade-in"> </li>);
+
+    if (this.state.matches) {
+      listElements = this.state.matches.map((match, index) => {
+        return (
+          <li className="slide-up-fade-in" key={index + 1}>
+            Match <b>{match.name}</b> won by <b>{match.winner}</b></li>
+        );
+      });
+    }
     return (
       <div className="layout-row">
         <div className="section-title">Select Year</div>
@@ -33,8 +56,8 @@ export default class FootballMatchesData extends Component {
                   'active': this.state.selectedYear === year
                 })
               }
-              onClick={this.onClick(year)}
-              key={year}>
+                onClick={this.onClick(year)}
+                key={year}>
                 <a>{year}</a>
               </li>
             )
@@ -43,10 +66,12 @@ export default class FootballMatchesData extends Component {
 
         <section className="content">
           <section>
-            <div className="total-matches" data-testid="total-matches"></div>
-            
+            <div className="total-matches" data-testid="total-matches">
+              {this.state.matches ? this.state.totalMatches : ''}
+            </div>
+
             <ul className="mr-20 matches styled" data-testid="match-list">
-              <li className="slide-up-fade-in"> </li>
+              {listElements}
             </ul>
           </section>
 
