0938aeb9fc29c5cdee2c1b4cd04b6176eeb328d5
herevrush <herevrush@yahoo.com>
2021-01-05 19:26:31 +1100

    adding condition for no data returned

--- src/components/football-data/index.js
+++ src/components/football-data/index.js
@@ -34,13 +34,14 @@ export default class FootballMatchesData extends Component {
 
   render() {
     var years = [2011, 2012, 2013, 2014, 2015, 2016, 2017];
+
     let listElements = (<li className="slide-up-fade-in"> </li>);
 
     if (this.state.matches) {
       listElements = this.state.matches.map((match, index) => {
         return (
           <li className="slide-up-fade-in" key={index + 1}>
-            Match <b>{match.name}</b> won by <b>{match.winner}</b></li>
+            Match {match.name} won by {match.winner}</li>
         );
       });
     }
@@ -66,16 +67,24 @@ export default class FootballMatchesData extends Component {
 
         <section className="content">
           <section>
-            <div className="total-matches" data-testid="total-matches">
-              {this.state.matches ? this.state.totalMatches : ''}
-            </div>
+            {this.state.selectedYear && this.state.totalMatches > 0 &&
+              <div className="total-matches" data-testid="total-matches">
+                Total matches: {this.state.totalMatches}
+              </div>
+            }
 
-            <ul className="mr-20 matches styled" data-testid="match-list">
-              {listElements}
-            </ul>
+            {this.state.matches.length > 0 &&
+              <ul className="mr-20 matches styled" data-testid="match-list">
+                {listElements}
+              </ul>
+            }
+
           </section>
 
-          <div data-testid="no-result" className="slide-up-fade-in no-result"></div>
+
+          {this.state.selectedYear && this.state.matches.length === 0 &&
+            <div data-testid="no-result" className="slide-up-fade-in no-result">No Matches Found</div>
+          }
         </section>
       </div>
     );
